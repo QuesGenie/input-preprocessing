f0722b00b14b47ca90a2371affb84fef62298cae
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2025-03-11 13:49:47 +0200

    Update core.py

--- documents/utils/core.py
+++ documents/utils/core.py
@@ -8,28 +8,25 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from input_preprocessing.documents.utils.retriever import Retriever
 
 #Json utilities
-@staticmethod
 def add_text_to_json_format(text,slide):
     slide["content"].append({
         "type": "text",
         "text": text,
     })
 
-@staticmethod
 def add_image_to_json_format(slide,image_tuple):
     slide["content"].append({
         "type": "image",
         "placeholder": f"[Image {image_tuple[1]+1}]",
         "image_path": image_tuple[2]
     })
-@staticmethod
+
 def add_image_text_to_json_format(slide,ocr_text):
     slide["content"].append({
         "type": "image",
         "ocr_text": ocr_text 
     })
             
-@staticmethod
 def write_json_to_file(data, filename):
     try:
         with open(filename, "w", encoding="utf-8") as json_file:
